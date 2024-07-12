390fa999a771aadf70cc4086d2dc18d048f50943
HarenKei <seungmin1266@gmail.com>
2024-07-12 14:48:49 +0900

    Feat: Implement contents card for empty generated trips

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -1,6 +1,9 @@
+"use client";
 import MyPageLayout from "./layout";
 import style from "./myPage.module.css";
 import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
 
 /**
  작성자 : 정승민
@@ -9,6 +12,8 @@ import Link from "next/link";
 */
 
 const MyPageMain: React.FC = () => {
+  const [recentData, setRecentData] = useState<any>(null);
+
   return (
     <div className={style.myPageContentsContainer}>
       <div className={style.myPageContentsTitle}>
@@ -17,7 +22,21 @@ const MyPageMain: React.FC = () => {
       </div>
 
       <div className={style.myPageContents}>
-        <h1>최근 생성한 여정 페이지</h1>
+        <div className={style.none_data_card}>
+          <Image
+            src={"/images/none_data_airplane.png"}
+            width={150}
+            height={158}
+            alt={"none data"}
+          />
+          <div className={style.card_description}>
+            <p id={style.none_data_title}>아직 생성된 여정이 없습니다.</p>
+            <p id={style.none_data_description}>
+              트리플러터에서 새로운 여행을 계획해보세요!
+            </p>
+            <Link href={"/trip-planning"}>여행 계획하러 가기 &gt;</Link>
+          </div>
+        </div>
       </div>
     </div>
   );
