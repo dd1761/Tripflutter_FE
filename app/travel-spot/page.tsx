163edfd5432fa1dc1d1743959b08833cc5105310
KIMDONGBEEN <dd1761@naver.com>
2024-07-31 11:33:34 +0900

    test

--- app/travel-spot/page.tsx
+++ app/travel-spot/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect, useRef } from "react";
 import styles from "./TravelSpot.module.css";
+import GoogleMapComponent from "@/app/components/GoogleMap/GoogleMapComponent";
 
 const TravelSpot: React.FC = () => {
   const images = [
@@ -58,12 +59,16 @@ const TravelSpot: React.FC = () => {
     );
   };
 
+
+  const locations = [
+    {
+      lat: 35.7149,
+      lng: 139.7967,
+    },
+  ]
+
   return (
     <div className={styles.container}>
-      <header className={styles.header}>
-        <h1 className={styles.title}>TRIPLE</h1>
-        <div className={styles.myReservation}>내 예약</div>
-      </header>
       <main className={styles.main}>
         <h2 className={styles.spotName}>센소지</h2>
         <div className={styles.spotInfo}>
@@ -78,49 +83,89 @@ const TravelSpot: React.FC = () => {
           <span className={styles.photographer}>아사쿠사</span>
         </div>
         <div
-          className={styles.imageContainer}
-          onTouchStart={handleTouchStart}
-          onTouchMove={handleTouchMove}
-          onTouchEnd={handleTouchEnd}
-          ref={carouselRef}
+            className={styles.imageContainer}
+            onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
+            onTouchEnd={handleTouchEnd}
+            ref={carouselRef}
         >
           <div
-            className={styles.imageWrapper}
-            style={{
-              transform: `translateX(-${currentImageIndex * 100}%)`,
-              transition: "transform 0.3s ease-out",
-            }}
+              className={styles.imageWrapper}
+              style={{
+                transform: `translateX(-${currentImageIndex * 100}%)`,
+                transition: "transform 0.3s ease-out",
+              }}
           >
             {images.map((image, index) => (
-              <img
-                key={index}
-                src={image}
-                alt={`센소지 사진 ${index + 1}`}
-                className={styles.image}
-              />
+                <img
+                    key={index}
+                    src={image}
+                    alt={`센소지 사진 ${index + 1}`}
+                    className={styles.image}
+                />
             ))}
           </div>
           <div className={styles.imageCount}>{`${currentImageIndex + 1} / ${
-            images.length
+              images.length
           }`}</div>
         </div>
         <div className={styles.actions}>
           <button className={styles.actionButton}>
-            <i className="far fa-heart"></i> 위시리스트
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                 stroke="currentColor" className="size-6"
+                 style={{width: '25px', height: '25px'}}
+            >
+              <path strokeLinecap="round" strokeLinejoin="round"
+                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"/>
+            </svg>
+            저장하기
           </button>
           <button className={styles.actionButton}>
-            <i className="fas fa-check"></i> 방문완료
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                 stroke="currentColor" className="size-6"
+                 style={{width: '25px', height: '25px'}}
+            >
+              <path strokeLinecap="round" strokeLinejoin="round"
+                    d="M6.75 2.994v2.25m10.5-2.25v2.25m-14.252 13.5V7.491a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v11.251m-18 0a2.25 2.25 0 0 0 2.25 2.25h13.5a2.25 2.25 0 0 0 2.25-2.25m-18 0v-7.5a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v7.5m-6.75-6h2.25m-9 2.25h4.5m.002-2.25h.005v.006H12v-.006Zm-.001 4.5h.006v.006h-.006v-.005Zm-2.25.001h.005v.006H9.75v-.006Zm-2.25 0h.005v.005h-.006v-.005Zm6.75-2.247h.005v.005h-.005v-.005Zm0 2.247h.006v.006h-.006v-.006Zm2.25-2.248h.006V15H16.5v-.005Z"/>
+            </svg>
+            일정추가
           </button>
           <button className={styles.actionButton}>
-            <i className="far fa-edit"></i> 후기쓰기
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                 stroke="currentColor" className="size-6"
+                 style={{width: '25px', height: '25px'}}
+            >
+              <path strokeLinecap="round" strokeLinejoin="round"
+                    d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"/>
+            </svg>
+            후기쓰기
           </button>
           <button className={styles.actionButton}>
-            <i className="fas fa-share-alt"></i> 공유하기
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                 stroke="currentColor" className="size-6"
+                 style={{width: '25px', height: '25px'}}
+            >
+              <path strokeLinecap="round" strokeLinejoin="round"
+                    d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15"/>
+            </svg>
+            공유하기
           </button>
         </div>
+
         <p className={styles.description}>
           일본 전통 문화가 살아 숨쉬는, 도쿄에서 가장 오래된 사원
         </p>
+        <div className={styles.googleMap}>
+          <GoogleMapComponent
+              mapContainerStyle={mapContainerStyle}
+              center={center}
+              locations={locations}
+          />
+          <div className={styles.mapDescription}>
+            <p>주소</p>
+            <p>111-0032 도쿄도 台東区 浅草2-3-1</p>
+          </div>
+        </div>
         <div className={styles.reviewSection}>
           <h3 className={styles.reviewTitle}>리뷰 5</h3>
           <div className={styles.reviewItem}>
@@ -139,3 +184,13 @@ const TravelSpot: React.FC = () => {
 };
 
 export default TravelSpot;
+
+const mapContainerStyle: React.CSSProperties = {
+  width: "100%",
+  height: "400px",
+};
+
+const center = {
+  lat: 35.6895,
+  lng: 139.6917,
+};
